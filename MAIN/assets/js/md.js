4130fdbca3acd66e975bce3623949f04fffb0725
bt-glv <brunotg02@gmail.com>
2022-05-29 12:06:05 -0300

    major changes to md.js

--- MAIN/assets/js/md.js
+++ MAIN/assets/js/md.js
@@ -27,7 +27,7 @@ const calc_congruency_mk1 = (el0, el1, x) => {
     if(d1 == d2){
 
         return {
-            el0_c: `${el0} % ${x} = ${d1}`,
+                el0_c: `${el0} % ${x} = ${d1}`,
                 el0_r: `${d1}`,
 
                 el1_c: `${el1} % ${x} = ${d2}`,
@@ -58,15 +58,60 @@ const calc_congruency_mk1 = (el0, el1, x) => {
 
 
 
+const congruency_main = (i_n1,i_n2,i_mod, answer_main, answer_calc1, anwwer_calc2) => {
+
+    const n1 = document.getElementById(i_n1).textContent
+    const n2 = document.getElementById(i_n2).textContent
+    const mod = document.getElementById(i_mod).textContent
+    
+    if((check_if_integer(n1.textContent)) * (check_if_integer(n2.textContent)) * (check_if_integer(mod.textContent))){
+
+        const res = calc_congruency_mk1(n1,n2,mod)
+
+        
+        if(res.r){document.getElementById(answer_main).textContent = "Congruênte" }  
+        else     {document.getElementById(answer_main).textContent =  "Incongruente"  }
+
+        document.getElementById(answer_calc1).textContent = res.el0_c
+        document.getElementById(answer_calc2).textContent = res.el2_c
+
+
+        result_addToList(res)
+
+    }else{
+
+        // todo: alert message here
+    }
+    
+    
+
+    
+
+}
+
 
 //
 //  Others
 //
 
 
-const clearList = (listID = "result_list") => {
+// todo: make a function to clear the results-page list
+const result_clearList = (listID = "result_list") => {
 
+    
+    
     list = document.getElementById(listID)
-    list.
+    
 
 }
+
+
+// todo: make a function to add all of the results to the result-page list
+const result_addToList = (results)=>{
+
+
+
+}
+
+
+
