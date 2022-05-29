117df433a914f7a73cd5758d8f2894754f49d7e7
bt-glv <brunotg02@gmail.com>
2022-05-28 22:48:48 -0300

    Finished the basic design of the interface. Now finishing  javascript

--- MAIN/assets/js/md.js
+++ MAIN/assets/js/md.js
@@ -41,10 +41,10 @@ const calc_congruency_mk1 = (el0, el1, x) => {
     else {
 
         return {
-            el0_c: `${el0} / ${x} = ${d1}`,
+            el0_c: `${el0} % ${x} = ${d1}`,
             el0_r: `${d1}`,
 
-            el1_c: `${el1} / ${x} = ${d2}`,
+            el1_c: `${el1} % ${x} = ${d2}`,
             el1_r: `${d2}`,
         
             m: `${d1} ≠ ${d2}`,
@@ -53,5 +53,20 @@ const calc_congruency_mk1 = (el0, el1, x) => {
 
     }
 
+
+}
+
+
+
+
+//
+//  Others
+//
+
+
+const clearList = (listID = "result_list") => {
+
+    list = document.getElementById(listID)
+    list.
 
 }
